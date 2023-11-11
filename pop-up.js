55e50bcc67abcf80664e7a53bea9fda4037d1016
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2023-11-11 20:17:11 +0200

    addes pop ups and made them responsive

---
+++ pop-up.js
@@ -0,0 +1,15 @@
+function toggle1(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    const popupBonded = document.querySelector('.popup-bonded');
+    popupBonded.classList.toggle('active');
+}
+
+function toggle2(){
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    const popupBonded = document.querySelector('.popup-triple-mash');
+    popupBonded.classList.toggle('active');
+}
